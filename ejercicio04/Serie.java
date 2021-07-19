020718fd3f9e2a74d3f52d42d5d8ca5886aec96e
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-07-19 08:16:50 +0200

    Update Serie.java

--- ejercicio04/Serie.java
+++ ejercicio04/Serie.java
@@ -2,4 +2,44 @@ package ejercicio04;
 
 public class Serie {
 
+	// Atributos del objeto.
+	protected String titulo, genero, creador;
+	protected boolean entregado;
+	protected int numTemporadas;
+
+	// Valores por defecto.
+	protected final int TEMPDEF = 3;
+	protected final boolean ENTDEF = false;
+
+	public Serie() {
+		titulo = "";
+		genero = "";
+		creador = "";
+		entregado = ENTDEF;
+		numTemporadas = TEMPDEF;
+
+	}
+
+	public Serie(String titulo, String creador) {
+		this.titulo = titulo;
+		this.creador = creador;
+		this.genero = "";
+		entregado = ENTDEF;
+		numTemporadas = TEMPDEF;
+	}
+
+	public Serie(String titulo, String genero, String creador, boolean entregado, int numTemporadas) {
+		this.titulo = titulo;
+		this.genero = genero;
+		this.creador = creador;
+		this.entregado = entregado;
+		this.numTemporadas = numTemporadas;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
 }
