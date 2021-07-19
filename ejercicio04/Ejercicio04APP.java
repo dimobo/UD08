a58188a54098ccd2d1b1899964b8735ef12a87e4
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-07-19 18:15:32 +0200

    04 ok

--- ejercicio04/Ejercicio04APP.java
+++ ejercicio04/Ejercicio04APP.java
@@ -1,9 +1,20 @@
 package ejercicio04;
 
 public class Ejercicio04APP {
-	
+
 	public static void main(String[] args) {
-		
+
+		// Aqui creamos objetos de la clase Serie.
+		Serie arraySeries[] = new Serie[3];
+		arraySeries[0] = new Serie();
+		arraySeries[1] = new Serie("Fumetsu no anata e", "Drama");
+		arraySeries[2] = new Serie("Fooly Cooly", "Comedia", "Hajime Ueda", true, 4);
+
+		// En estebucle nos imprime por pantalla los objetos de la clase Serie.
+		for (int i = 0; i < arraySeries.length; i++) {
+			System.out.println(arraySeries[i].toString());
+		}
+
 	}
 
 }
--- ejercicio04/Serie.java
+++ ejercicio04/Serie.java
@@ -11,6 +11,7 @@ public class Serie {
 	protected final int TEMPDEF = 3;
 	protected final boolean ENTDEF = false;
 
+	// Este es el constructor por defecto.
 	public Serie() {
 		titulo = "";
 		genero = "";
@@ -20,6 +21,8 @@ public class Serie {
 
 	}
 
+	// Este es el constructor para cuando mandemos solo el titulo y el creador de la
+	// serie.
 	public Serie(String titulo, String creador) {
 		this.titulo = titulo;
 		this.creador = creador;
@@ -28,6 +31,7 @@ public class Serie {
 		numTemporadas = TEMPDEF;
 	}
 
+	// Este es el constructor con todos los parametros.
 	public Serie(String titulo, String genero, String creador, boolean entregado, int numTemporadas) {
 		this.titulo = titulo;
 		this.genero = genero;
@@ -35,11 +39,53 @@ public class Serie {
 		this.entregado = entregado;
 		this.numTemporadas = numTemporadas;
 	}
-	
+
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
+		return "Titulo de la Serie: " + this.titulo + "\nGenero de la serie: " + this.genero + "\nCreador de la serie: "
+				+ this.creador + "\nSerie entregada: " + this.entregado + "\nNumero de temporadas: "
+				+ this.numTemporadas + "\n\n";
+	}
+
+	// Zona getters y setters.
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCreador() {
+		return creador;
+	}
+
+	public void setCreador(String creador) {
+		this.creador = creador;
+	}
+
+	public boolean isEntregado() {
+		return entregado;
+	}
+
+	public void setEntregado(boolean entregado) {
+		this.entregado = entregado;
+	}
+
+	public int getNumTemporadas() {
+		return numTemporadas;
+	}
+
+	public void setNumTemporadas(int numTemporadas) {
+		this.numTemporadas = numTemporadas;
 	}
 
 }
